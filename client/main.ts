cda84262dde1971f0891d7bbeefcd7df78c17632
zanot <shichon64@msn.com>
2016-05-28 00:52:13 +0200

    Add App component

---
+++ client/main.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { App } from '../imports/components/app';
+
+bootstrap( App );
---
+++ imports/components/app.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app',
+    templateUrl: '/imports/components/app.html'
+})
+export class App {}
